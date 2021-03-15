dd849a77ce3e21335712e8b370dbbe2a2de6a76e
poonkawin <poonkawin8484@gmail.com>
2021-03-16 00:25:57 +0530

    heroku_django in settings.py

--- firstProject/settings.py
+++ firstProject/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,3 +145,4 @@ STATICFILES_DIRS = (
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+django_heroku.settings(locals())
